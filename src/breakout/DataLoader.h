9b1fc46be20ed6dac0d6566d88ccb5f0c0111073
Jordi Amorós <jam80@alu.ua.es>
2018-12-06 01:04:41 +0100

    [FIX] Removed virtual method and run_agent.sh

--- src/breakout/DataLoader.h
+++ src/breakout/DataLoader.h
@@ -15,7 +15,7 @@ class DataLoader {
     public:
         DataLoader(string filename, vector<int> &topology);
         ~DataLoader() { delete this->net; this->net = NULL; }
-        virtual void trainNN(unsigned nInputs, unsigned nOutputs, unsigned epochs);
+        void trainNN(unsigned nInputs, unsigned nOutputs, unsigned epochs);
         NeuralNetwork* getNN() { return this->net; }
         void setFilename(string fn) { this->filename = fn; }
         vector<double> getPrediction(vector<double> inputs);
